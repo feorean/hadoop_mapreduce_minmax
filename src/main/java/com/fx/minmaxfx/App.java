e42e76065003cae772426b64a40767b042e95a15
Khalid Mammadov <xmamedov@gmail.com>
2017-12-17 23:42:06 +0000

    latest

--- src/main/java/com/fx/minmaxfx/App.java
+++ src/main/java/com/fx/minmaxfx/App.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -82,7 +81,7 @@ public class App {
     //job.setCombinerClass(MinMaxReducer.class);
     job.setReducerClass(MinMaxReducer.class);
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(DoubleWritable.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
